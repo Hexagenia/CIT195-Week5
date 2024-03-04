9db49da2d0dcacedd822d0b52416ca2494d2f7cf
Colin Elliott <79231253+Hexagenia@users.noreply.github.com>
2024-03-03 19:58:57 -0500

    Add files via upload

---
+++ AbstractAnimalClass/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace AbstractAnimalClass
+{
+    abstract class Animal
+    {
+        // Property
+        public abstract string Name { get; set; }
+
+        // Additional fields
+        public abstract int Age { get; set; }
+        public abstract string Species { get; set; }
+
+        // Methods
+        public abstract string Describe();
+        public string whatAmI()
+        {
+            return "I am an animal";
+        }
+    }
+
+    class Dog : Animal
+    {
+        // Properties
+        public override string Name { get; set; }
+        public override int Age { get; set; }
+        public override string Species { get; set; }
+
+        // Constructors
+        public Dog() : this("", 0, "")
+        {
+        }
+
+        public Dog(string name, int age, string species)
+        {
+            Name = name;
+            Age = age;
+            Species = species;
+        }
+
+        // Override the Describe method
+        public override string Describe()
+        {
+            return $"I am a {Species}\nMy name is {Name}, and I am {Age} years old.";
+        }
+    }
+
+    class Cat : Animal
+    {
+        // Properties
+        public override string Name { get; set; }
+        public override int Age { get; set; }
+        public override string Species { get; set; }
+
+        // Constructors
+        public Cat() : this("", 0, "")
+        {
+        }
+
+        public Cat(string name, int age, string species)
+        {
+            Name = name;
+            Age = age;
+            Species = species;
+        }
+
+        // Override the Describe method
+        public override string Describe()
+        {
+            return $"I am a {Species}\nMy name is {Name}, and I am {Age} years old.";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Dog object using the default constructor
+            Dog dog1 = new Dog();
+            dog1.Name = "Colin";
+            dog1.Age = 3;
+            dog1.Species = "Dog";
+            Console.WriteLine(dog1.whatAmI());
+            Console.WriteLine(dog1.Describe());
+            Console.WriteLine();
+
+            // Create Cat object using the parameterized constructor
+            Cat cat1 = new Cat("Parmida", 2, "Cat");
+            Console.WriteLine(cat1.whatAmI());
+            Console.WriteLine(cat1.Describe());
+        }
+    }
+}
---
+++ nascar/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace nascar
+{
+    // Interface representing a NASCAR driver
+    interface INascarDriver
+    {
+        // Properties
+        string Name { get; }
+        int CarNumber { get; }
+        string Team { get; }
+        string Manufacturer { get; }
+
+        // Method to display driver information
+        void DisplayInfo();
+    }
+
+    // Concrete class representing a NASCAR driver
+    class NascarDriver : INascarDriver
+    {
+        // Properties
+        public string Name { get; }
+        public int CarNumber { get; }
+        public string Team { get; }
+        public string Manufacturer { get; }
+
+        // Constructor
+        public NascarDriver(string name, int carNumber, string team, string manufacturer)
+        {
+            Name = name;
+            CarNumber = carNumber;
+            Team = team;
+            Manufacturer = manufacturer;
+        }
+
+        // Implementing the DisplayInfo method
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"{Name} is driving the #{CarNumber} {Team} {Manufacturer}.");
+            Console.WriteLine($"{Name} is the driver, {CarNumber} is his number, {Team} is the team he is a member of, and {Manufacturer} is his team's manufacturer.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a NASCAR driver object
+            INascarDriver driver = new NascarDriver("Denny Hamlin", 11, "Joe Gibbs Racing", "Toyota Camry");
+
+            // Display driver information
+            driver.DisplayInfo();
+        }
+    }
+}
